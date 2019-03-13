a7f91481f6c7346317fe32cd05263cfaff3370a7
juansebascb <juansebascb2@gmail.com>
2019-03-13 02:53:39 -0400

    Finished calculator history.

--- docs/main.js
+++ docs/main.js
@@ -2,10 +2,60 @@ const calculator = document.getElementById('calculator');
 const keys = calculator.querySelector('.pressable');
 const display = calculator.querySelector('#display');
 const memoryRecall = keys.querySelector('.recall');
+
 decimalPressed = false;
+
+let history = {
+    calculationStack : [],
+    displayStack: function() {
+        for (i = 1; i <= this.calculationStack.length; i++){
+            para = document.createElement("p");
+            node = document.createTextNode(this.calculationStack[this.calculationStack.length - i]);
+            para.appendChild(node);
+            element = document.getElementById("history");
+            element.appendChild(para);
+        }
+    },
+    addToTop: function(str) {
+        this.calculationStack.push(str);
+        localStorage.setItem("pastHistory", JSON.stringify(this));
+        this.displayLatest();
+    },
+    displayLatest: function() {
+        if (this.calculationStack.length == 1){
+            this.displayStack();
+        } else {
+            var para = document.createElement("p");
+            var node = document.createTextNode(this.calculationStack[this.calculationStack.length - 1]);
+            para.appendChild(node);
+            var element = document.getElementById("history");
+            var child = element.getElementsByTagName("p");
+            element.insertBefore(para,child[0]);
+        }
+    },
+    size: function() {
+        return this.calculationStack.length;
+    },
+    clearHistory: function() {
+        var element = document.getElementById("history");
+
+        for (i = 0; i <= this.calculationStack.length; i++){
+            element.removeChild(element.childNodes[2]);
+        }
+
+        this.calculationStack = [];
+        localStorage.setItem("pastHistory", "");
+    }
+}
 
 if (localStorage.getItem("MS")) {
     memoryRecall.classList.add('somethingSaved');
+}
+
+if (localStorage.getItem("pastHistory")) {
+    var temp = JSON.parse(localStorage.getItem("pastHistory"));
+    history.calculationStack = temp.calculationStack
+    history.displayStack();
 }
 
 keys.addEventListener('click', e => {
@@ -90,21 +140,29 @@ keys.addEventListener('click', e => {
 });
 
 function calculate() {
+    str = "";
+
     switch (calculator.dataset.operator) {
         case 'add':
+            str = calculator.dataset.firstValue + " + " + display.value;
             display.value = parseFloat(calculator.dataset.firstValue) + parseFloat(display.value);
             break;
         case 'subtract':
+            str = calculator.dataset.firstValue + " - " + display.value;
             display.value = parseFloat(calculator.dataset.firstValue) - parseFloat(display.value);
             break;
         case 'multiply':
+            str = calculator.dataset.firstValue +  " * " + display.value;
             display.value = parseFloat(calculator.dataset.firstValue) * parseFloat(display.value);
             break;
         case 'divide':
+            str = calculator.dataset.firstValue + " / " + display.value;
             display.value = parseFloat(calculator.dataset.firstValue) / parseFloat(display.value);
             break;
     };
     calculator.dataset.firstValue = '';
+    str += " = " + display.value;
+    history.addToTop(str);
 }
 
 function clear() {
