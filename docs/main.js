c5703fc069fdb4e18d53acd02dbe81b105988a9e
juansebascb <juansebascb2@gmail.com>
2019-03-14 04:25:35 -0400

    Added event for loading in and for number key presses as well as function key presses

--- docs/main.js
+++ docs/main.js
@@ -48,14 +48,18 @@ let history = {
     }
 }
 
-if (localStorage.getItem("MS")) {
-    memoryRecall.classList.add('somethingSaved');
+function displayMemory() {
+    if (localStorage.getItem("MS")) {
+        memoryRecall.classList.add('somethingSaved');
+    }
 }
 
-if (localStorage.getItem("pastHistory")) {
-    var temp = JSON.parse(localStorage.getItem("pastHistory"));
-    history.calculationStack = temp.calculationStack
-    history.displayStack();
+function displayHistory() {
+    if (localStorage.getItem("pastHistory")) {
+        var temp = JSON.parse(localStorage.getItem("pastHistory"));
+        history.calculationStack = temp.calculationStack
+        history.displayStack();
+    }
 }
 
 keys.addEventListener('click', e => {
@@ -170,3 +174,60 @@ function clear() {
     display.value = '0';
     calculator.dataset.firstValue = '';
 }
+
+window.addEventListener ('keypress', e => {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+        case 47:
+            document.getElementById('%').click();
+        case 46:
+            document.getElementById('.').click();
+            break;
+        case 13:
+        case 61:
+            document.getElementById('equals').click();
+            break;
+        case 47:
+            document.getElementById('/').click();
+            break;
+        case 42:
+            document.getElementById('*').click();
+            break;
+        case 45:
+            document.getElementById('-').click();
+            break;
+        case 43:
+            document.getElementById('+').click();
+            break;
+        case 57:
+            document.getElementById('9').click();
+            break;
+        case 56:
+            document.getElementById('8').click();
+            break;
+        case 55:
+            document.getElementById('7').click();
+            break;
+        case 54:
+            document.getElementById('6').click();
+            break;
+        case 53:
+            document.getElementById('5').click();
+            break;
+        case 48:
+            document.getElementById('0').click();
+            break;
+        case 49:
+            document.getElementById('1').click();
+            break;
+        case 50:
+            document.getElementById('2').click();
+            break;
+        case 51:
+            document.getElementById('3').click();
+            break;
+        case 52:
+            document.getElementById('4').click();
+            break;
+    };
+});
