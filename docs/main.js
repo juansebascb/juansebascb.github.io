efd1ff75c1f02a4f3b0c1e1d860750c9998d8fc5
juansebascb <juansebascb2@gmail.com>
2019-03-15 16:33:07 -0400

    Revert "Changed key problem"
    This reverts commit e41b26f4748d73d1621df71f3dd1888d47447894.

--- docs/main.js
+++ docs/main.js
@@ -178,6 +178,8 @@ function clear() {
 window.addEventListener ('keypress', e => {
     console.log(e.keyCode);
     switch (e.keyCode) {
+        case 47:
+            document.getElementById('%').click();
         case 46:
             document.getElementById('.').click();
             break;
